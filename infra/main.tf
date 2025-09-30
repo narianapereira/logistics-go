a5eca81f3c3bda84c9c7d0553a53641518cdfe30
Nariana <nariana.pereira@outlook.com>
2025-09-30 18:16:08 -0300

    refactor: separates deployment from service

--- infra/main.tf
+++ infra/main.tf
@@ -25,21 +25,33 @@ provider "kubernetes" {
 
 # --- YAML AND DEPLOY ---
 
-data "local_file" "manifest_yaml" {
+data "local_file" "deployment_yaml" {
   filename = "manifests/deployment.yaml"
+}
+
+data "local_file" "service_yaml" {
+  filename = "manifests/service.yaml"
 }
 
 locals {
   # Substitutes the IMAGE_PLACEHOLDER for the actual tag for the docker image
-  rendered_manifest = replace(
-    data.local_file.manifest_yaml.content,
+  rendered_deployment = replace(
+    data.local_file.deployment_yaml.content,
     "IMAGE_PLACEHOLDER",
     var.app_docker_image
   )
+
+  rendered_service = data.local_file.service_yaml.content
 }
 
-resource "kubernetes_manifest" "logistics_app" {
-  manifest = yamldecode(local.rendered_manifest)
+resource "kubernetes_manifest" "logistics_app_deployment" {
+  manifest = yamldecode(local.rendered_deployment)
+}
+
+resource "kubernetes_manifest" "logistics_app_service" {
+  manifest = yamldecode(local.rendered_service)
+
+  depends_on = [kubernetes_manifest.logistics_app_deployment]
 }
 
 # --- OUTPUT ---
@@ -48,8 +60,7 @@ data "kubernetes_service" "logistics_service_data" {
   metadata {
     name = "logistics-service"
   }
-  depends_on = [kubernetes_manifest.logistics_app]
-
+  depends_on = [kubernetes_manifest.logistics_app_service]
 }
 
 # Prints external api in actions
